aee7b1aeb3140665865677182e38f0404256e158
tquach8 <tan_q8@live.com>
2015-11-25 15:49:06 -0700

    Added functionality to the calendar for events
    To test the current functionality, click on the Nov 25/2015 date.
    -- Bug
    + Image margins are added in a weird way (might be different image
    sizes)
    -- Still need to add
    + Different events
    + Button functionality

--- 481kiosk/DiscoverWindow.xaml.cs
+++ 481kiosk/DiscoverWindow.xaml.cs
@@ -19,9 +19,82 @@ namespace _481kiosk
     /// </summary>
     public partial class DiscoverWindow : Window
     {
+        private Dictionary<string, List<Events>> eventListing;
+
         public DiscoverWindow()
         {
+            List<Events> novEvents = new List<Events>();
+            novEvents.Add(new Events("Calgary Flames", "Calgary flames face off against the league leading team Chicago Blackhawks", "Calgary-Flames.jpg"));
+            novEvents.Add(new Events("Illuminasia", "Exotic lantern festival from different parts of asia", "illuminasia.jpg"));
+            novEvents.Add(new Events("Calgary Stampede", "Famous cowboy festival for all ages", "stampede_logo.png"));
             InitializeComponent();
+            eventListing = new Dictionary<string, List<Events>>();
+            eventListing.Add("11/25/2015", novEvents);
+
+        }
+
+        private void Selection_Change(object sender, SelectionChangedEventArgs e)
+        {
+
+            // Iterate through the SelectedDates collection and display the
+            // dates selected in the Calendar control.
+            foreach (DateTime day in calendar1.SelectedDates)
+            {
+                if(eventListing.ContainsKey(day.Date.ToShortDateString()))
+                {
+                    List<Events> tempEventListing = eventListing[day.Date.ToShortDateString()];
+                    int count = tempEventListing.Count();
+                    //For loop to create a list of buttons depending on how many events there are
+                    List<Button> buttons = new List<Button>();
+                    for(int i = 0; i < count; i++)
+                    {
+
+                        Grid grid = new Grid();
+                        StackPanel stackpanel = new StackPanel();
+
+                        Image eventImage = new Image();
+                        BitmapImage bitmap = new BitmapImage();
+                        eventImage.Source = new BitmapImage(new Uri(tempEventListing[i].getImg(), UriKind.Relative));
+                        eventImage.HorizontalAlignment = HorizontalAlignment.Left;
+                        eventImage.Width = 150;
+                        eventImage.Height = 100;
+                        Thickness margin = eventImage.Margin;
+                        margin.Left = -160;
+                        margin.Top = 0;
+                        margin.Right = 0;
+                        margin.Bottom = -9;
+                        eventImage.Margin = margin;
+
+                        TextBlock eventName = new TextBlock();
+                        eventName.FontWeight = FontWeights.Bold;
+                        eventName.Text = tempEventListing[i].getName();
+
+                        TextBlock eventDesc = new TextBlock();
+                        eventDesc.Text = day.Date.ToShortDateString() + "\n" +tempEventListing[i].getDescription();
+                        eventDesc.TextWrapping = TextWrapping.Wrap;
+
+                        stackpanel.Children.Add(eventName);
+                        stackpanel.Children.Add(eventDesc);
+                        grid.Children.Add(eventImage);
+                        Grid.SetColumn(eventImage, 0);
+                        grid.Children.Add(stackpanel);
+                        Grid.SetColumn(stackpanel, 1);
+
+                        Button b = new Button();
+                        //b.Name = tempEventListing[i].getName();
+                        b.Content = grid;
+
+                        buttonStackpanel.Children.Add(b);
+                    }
+                }
+                else
+                {
+                    buttonStackpanel.Children.RemoveRange(1, buttonStackpanel.Children.Count - 1);
+                }
+
+
+
+            }
         }
     }
 }
---
+++ 481kiosk/Events.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _481kiosk
+{
+
+    public class Events
+    {
+        private string eventName;
+        private string eventDescription;
+        private string image;
+
+
+        public Events(string name, string desc, string img)
+        {
+            eventName = name;
+            eventDescription = desc;
+            image = img;
+        }
+
+        public string getName()
+        {
+            return eventName;
+        }
+        
+        public string getDescription()
+        {
+            return eventDescription;
+        }
+
+        public string getImg()
+        {
+            return image;
+        }
+    }
+
+
+}
